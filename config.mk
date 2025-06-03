499a6c8ea4f28f16d782ca3c85a5c39e8b4ac13d
shapirov103 <shapirov@amazon.com>
2025-06-03 14:20:18 -0400

    simplified configuration, removed dependency on cdk.contenxt.json, removed failure if .env file is not present

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # Common configuration
-include .env
-export $(shell sed 's/=.*//' .env)
+-include .env
+export $(shell if [ -f .env ]; then sed 's/=.*//' .env; fi)
 SHELL = /bin/bash
 _PWD = $(shell pwd | sed "s~/cygdrive/c~c:~")
 AWS_ACCOUNT = $(shell aws sts get-caller-identity --query Account --output text)
@@ -10,6 +10,9 @@ endif
 
 export AWS_REGION       ?= us-east-1
 export AWS_ECR_REGISTRY := $(AWS_ACCOUNT).dkr.ecr.$(AWS_REGION).amazonaws.com
+ifndef STACK_NAME
+export STACK_NAME := rtb-$(AWS_ACCOUNT)
+endif
 export ROOT_STACK_NAME := $(STACK_NAME)
 
 export AWS_PUBLIC_ECR_REGISTRY = public.ecr.aws/docker/library
